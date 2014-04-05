ccb78a964cc1f7552c1799205fd26ed81f2342dc
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-04-05 15:15:42 -0400

    less noise

--- ioclogserv/processor.py
+++ ioclogserv/processor.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import logging, logging.handlers
-
 import re, time
 
 from ConfigParser import SafeConfigParser as ConfigParser
--- ioclogserv/receiver.py
+++ ioclogserv/receiver.py
@@ -12,35 +12,27 @@ class IOCLogServer(LineOnlyReceiver):
     delimiter = b'\n'
     def __init__(self, coll):
         self.N, self.Nlim, self.paused, self.collector = 0, 30, False, coll
-        self.logstr = '<unassociated>'
-    def logPrefix(self):
-        return self.logstr
     def connectionMade(self):
         self.transport.setTcpKeepAlive(1)
         self.peer = self.transport.getPeer()
-        self.logstr = 'client %s'%self.peer
         self.collector.connect(self)
-        log.msg('connected')
+        log.msg('connected %s:%d'%(self.peer.host, self.peer.port))
     def connectionLost(self, reason=None):
         self.collector.disconnect(self)
-        log.msg('disconnected: %s'%reason)
-        self.logstr = 'client %s (disconnected)'%self.peer
+        log.msg('disconnected')
         self.peer = None
     def lineLengthExceeded(self, line):
         self.lineReceived('Line length exceeded')
     def lineReceived(self, line):
         if self.N >= self.Nlim:
-            log.msg('throttled')
             self.transport.pauseProducing()
             self.paused = True
             return
         self.N += 1
         self.collector.add(self, self.peer, line, time.time())
     def ack(self):
-        log.msg('ack')
         self.N = 0
         if self.paused:
-            log.msg('resumed')
             self.transport.resumeProducing()
             self.paused = False
 
